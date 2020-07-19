982bb81317edf471ece6b5c926ca36b5a63ef0fe
송용은 <song_6628@naver.com>
2020-07-19 16:29:48 +0900

    first

---
+++ playGround.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "gameNode.h"
+
+//#pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console")
+
+
+class playGround : public gameNode
+{
+private:
+public:
+	playGround();
+	~playGround();
+
+public:
+	virtual HRESULT init();
+	virtual void release();
+	virtual void update();
+	virtual void render();
+};
+
