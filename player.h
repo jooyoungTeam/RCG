f7b098f72a25b7977a9da62e450cdf7f06d13777
해라 강 <gofk852@naver.com>
2020-07-20 09:39:14 +0900

    player setting

--- player.h
+++ player.h
@@ -1,8 +1,44 @@
 #pragma once
 #include "gameNode.h"
+
+#pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console")
+
+class playerState;
+class idleState;
+class moveState;
+class jumpState;
+
+
+
 class player : public gameNode
 {
 private:
+	playerState* _state;
+	image* _img;
+	RECT _rc;
+	RECT _player;
+
+	int _probeBottom;
+	bool _isMove;
+	bool _isJumping;
+
+	float _x, _y;
+	float _frameX, _frameY;
+	float _playerX, _playerY;
+
+	bool _direction;		//left = 0, right = 1
+
+	float _jumpPower, _gravity;
+
+
+
+	animation* _playerMotion;
+
+
+	playerState* _idle;
+	playerState* _move;
+	playerState* _jump;
+
 
 public:
 	player() {}
@@ -13,5 +49,62 @@ public:
 	virtual void update();
 	virtual void release();
 public:
+
+	void setState(playerState* state)
+	{
+		this->_state = state;
+	}
+	float getX() { return _x; }
+	float getY() { return _y; }
+	void setX(float x) { _x = x; }
+	void setY(float y) { _y = y; }
+	void setPlayerX(float playerX) { _playerX = playerX; }
+	void setPlayerY(float playerY) { _playerY = playerY; }
+	float getPlayerX() { return _playerX; }
+	float getPlayerY() { return _playerY; }
+	void setAni(animation* ani, image* img) { _img = img;  _playerMotion = ani; _playerMotion->start(); }
+	bool getDirection() { return _direction; }
+	void setDirection(bool direction) { _direction = direction; }
+	bool getIsMove() { return _isMove; }
+	float getJumpPower() { return _jumpPower; }
+	float getGravity() { return _gravity; }
+	void setJumpPower(float jumpPower) { _jumpPower = jumpPower; }
+	void setGravity(float gravity) { _gravity = gravity; }
+	bool getIsJumping() { return _isJumping; }
+	void setIsJumping(bool jumping) { _isJumping = jumping; }
+	playerState* getIdleState() { return _idle; }
+	playerState* getMoveState() { return _move; }
+	playerState* getJumpState() { return _jump; }
 };
+
+
+class playerState
+{
+public:
+	virtual ~playerState() {}
+	virtual void update(player& player) {}
+public:
+	static idleState* idle;
+	static moveState* move;
+	static jumpState* jump;
+};
+
+class idleState : public playerState
+{
+public:
+	virtual void update(player& player) override;
+};
+
+class moveState : public playerState
+{
+public:
+	virtual void update(player& player) override;
+};
+
+class jumpState : public playerState
+{
+public:
+	virtual void update(player& player) override;
+};
+
 
